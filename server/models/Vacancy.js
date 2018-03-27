5bc1bb59c7faba92bb611eed0deeea2068550aa1
Prashant Singh <mprashantks@gmail.com>
2018-03-27 19:45:50 +0530

    Made minor modifications in Vacancy schema

--- server/models/Vacancy.js
+++ server/models/Vacancy.js
@@ -49,8 +49,10 @@ var VacancySchema = new mongoose.Schema({
                 required: true
               },
               vacancy: {
-                type: Number,
-                required: true,
+                number: {
+                  type: Number,
+                  required: true
+                },
                 positions: [{
                   designation: {
                     type: String,
@@ -62,6 +64,38 @@ var VacancySchema = new mongoose.Schema({
       }]
   }]
 });
+
+VacancySchema.statics.findVacantPositions = function () {
+  var Vacancy = this;
+  Vacancy.find({
+    
+  }).then((vacant_positions) => {
+    if(vacant_positions.length == 0) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      resolve(vacant_positions);
+    });
+  })
+};
+
+/*VacancySchema.statics.findVacantPositions = function (email, password) {
+  var Employee = this;
+  return Employee.findOne({email}).then((employee) => {
+    if(!employee) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, employee.password, (err, res) => {
+          if(res) {
+            resolve(employee);
+          } else {
+            reject();
+          }
+      })
+    });
+  });
+};*/
 
 
 var Vacancy = mongoose.model('Vacancy', VacancySchema);
